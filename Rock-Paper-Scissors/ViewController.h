63ad937cf98e1633f96a37bb768a6bdd7f080ae0
pjpjpj <877302410@qq.com>
2017-09-30 10:11:18 +0800

    Initial Commit

---
+++ Rock-Paper-Scissors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rock-Paper-Scissors
+//
+//  Created by pjpjpj on 2017/9/30.
+//  Copyright © 2017年 #incloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Rock-Paper-Scissors/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Rock-Paper-Scissors
+//
+//  Created by pjpjpj on 2017/9/30.
+//  Copyright © 2017年 #incloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
